5161cb3b4299486799b792308859d8bd3588d93c
Christian Calderon <Chrisc23@colostate.edu>
2025-02-04 21:44:22 -0700

    Added team name

--- server/src/main/java/com/tco/misc/Team.java
+++ server/src/main/java/com/tco/misc/Team.java
@@ -8,7 +8,7 @@ public class Team {
   String mission;
 
   public Team() {
-    name = "Team Name";
+    name = "Breaking The Matrix";
     number = "t41";
     mission = "Insert your team's mission statement here! Lorem ipsum odor amet, consectetuer adipiscing elit. Sociosqu nisi ut luctus dapibus platea justo justo. Diam ridiculus sem nisi consequat senectus sagittis tempus neque. Sem faucibus netus velit odio ridiculus porta. Sit vulputate sollicitudin penatibus dolor, velit eu molestie. Semper quis velit ridiculus bibendum elit. Vel sollicitudin eu quisque ligula felis eleifend, quis in curae. Metus convallis dis pellentesque posuere et sit suspendisse potenti. Lacinia dignissim duis vel urna dignissim pellentesque litora tempor. Netus vulputate commodo dolor aptent efficitur.";
   }
