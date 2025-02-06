2cda9479a03b06434d7f67d11163dd749647dfab
Christian Calderon <Chrisc23@colostate.edu>
2025-02-05 22:21:11 -0700

    Added Person's 1 name

--- server/src/main/java/com/tco/misc/People.java
+++ server/src/main/java/com/tco/misc/People.java
@@ -14,7 +14,7 @@ public class People extends ArrayList<Person> {
   }
 
   Person person1() {
-    final String name = "Team Member name";
+    final String name = "Christian Calderon";
     final String netid = "Team Member netid";
     final String hometown = "Team Member Hometown";
     final String bio = "Team Member bio";
