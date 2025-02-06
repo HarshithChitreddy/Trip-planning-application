e40f2583256f1237543328151b7974f504bd112f
Kjell Falk <kjell@berlin.cs.colostate.edu>
2025-02-06 15:12:58 -0700

    added my netID

--- server/src/main/java/com/tco/misc/People.java
+++ server/src/main/java/com/tco/misc/People.java
@@ -24,7 +24,7 @@ public class People extends ArrayList<Person> {
 
   Person person2() {
     final String name = "Kjell Falk";
-    final String netid = "Team Member netid";
+    final String netid = "kjell";
     final String hometown = "Team Member Hometown";
     final String bio = "Team Member bio";
 
