9be69871040f5a186c8e73502b5e30cf19ec53a2
Christian Calderon <Chrisc23@colostate.edu>
2025-02-20 16:03:00 -0700

    made a shell for vicenty Calculator

--- server/src/main/java/com/tco/misc/VicentyCalculator.java
+++ server/src/main/java/com/tco/misc/VicentyCalculator.java
@@ -3,12 +3,6 @@ package com.tco.misc;
 public class VicentyCalculator extends DistanceCalculator{
     @Override
     public long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
-        double lat1 = from.latitudeRadians();
-        double lon1 = from.longitudeRadians();
-        double lat2 = to.latitudeRadians();
-        double lon2 = to.longitudeRadians();
-
-        return Math.round(computeVicentyDistance(lat1,lon1,lat2,lon2,earthRadius));
-
+        
     }
 }
