2798fdeb09c2b86f81614e1ab029f8394cf2621f
Christian Calderon <Chrisc23@colostate.edu>
2025-02-20 16:09:41 -0700

    fixed error it needs to return something

--- server/src/main/java/com/tco/misc/VicentyCalculator.java
+++ server/src/main/java/com/tco/misc/VicentyCalculator.java
@@ -3,6 +3,7 @@ package com.tco.misc;
 public class VicentyCalculator implements DistanceCalculator{
     @Override
     public long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
-        
+        long totalDistance = 0
+        return totalDistance;
     }
 }
