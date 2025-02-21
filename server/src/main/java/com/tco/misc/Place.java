5256c4a6a2e8d037ae46eaaff94341c3b283b265
Kjell Falk <kjell@berlin.cs.colostate.edu>
2025-02-20 18:30:42 -0700

    aligned Place.java class with the sprint2 slide for Place.java

--- server/src/main/java/com/tco/misc/Place.java
+++ server/src/main/java/com/tco/misc/Place.java
@@ -1,6 +1,17 @@
-package com.tco.misc;
+package com.tco.requests;
 
-public class Place implements GeographicCoordinate {
-    public double latRadians();
-    public double lonRadians();
+import java.util.HashMap;
+import java.lang.Math;
+import com.tco.misc.GeographicCoordinate;
+
+public class Place extends HashMap<String, String> implements GeographicCoordinate {
+
+    public double latRadians() {
+        return 0.0;
+    }
+
+    public double lonRadians() {
+        return 0.0;
+    }
+
 }
