93630f9df9d102c2d7fb2788a7bf4ea4570b6004
Christian Calderon <Chrisc23@colostate.edu>
2025-02-21 18:25:43 -0700

    Fixed the spelling issue for Vincenty

--- server/src/main/java/com/tco/misc/VincentyCalculator.java
+++ server/src/main/java/com/tco/misc/VincentyCalculator.java
@@ -1,6 +1,6 @@
 package com.tco.misc;
 
-public class VicentyCalculator implements DistanceCalculator{
+public class VincentyCalculator implements DistanceCalculator{
     @Override
     public long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
         //just a placeholder so it can pass the test
