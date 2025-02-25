18ae96244f53e3bc2d4ad3aa0b3626c0f03afbfb
Harshith Reddy Chitreddy <reddy17@colostate.edu>
2025-02-25 01:21:01 -0700

    value returnted immediately without having to save it into any variable

--- server/src/main/java/com/tco/misc/VincentyCalculator.java
+++ server/src/main/java/com/tco/misc/VincentyCalculator.java
@@ -4,7 +4,6 @@ public class VincentyCalculator implements DistanceCalculator{
     @Override
     public long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
         //just a placeholder so it can pass the test
-        long totalDistance = 0;
-        return totalDistance;
+        return 0;
     }
 }
