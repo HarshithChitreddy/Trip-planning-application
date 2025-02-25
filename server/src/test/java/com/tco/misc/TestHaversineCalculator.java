11e0ae81d6451c97eab3601478c73e322c2fd605
Dakota Weathers <dnweath@colostate.edu>
2025-02-25 12:10:22 -0700

    Generate test class

---
+++ server/src/test/java/com/tco/misc/TestHaversineCalculator.java
@@ -0,0 +1,12 @@
+package com.tco.misc;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestHaversineCalculator {
+    /*
+     * Initial test class creation
+     */
+}
