51ce4087f7ee46cd9a71d9129727bd7c3af507a3
Christian Calderon <Chrisc23@colostate.edu>
2025-02-25 16:09:26 -0700

    Added some test Cases to Vincenty

--- server/src/test/java/com/tco/misc/TestVincentyCalculator.java
+++ server/src/test/java/com/tco/misc/TestVincentyCalculator.java
@@ -38,4 +38,44 @@ public class TestVincentyCalculator {
         long actualDistance = vincentyTest.between(geoTest1, geoTest2, radiusTest);
         assertEquals(0L, actualDistance);
     }
+    @DisplayName("chrisc23: Test Vincenty Calculator: Equatorial Distance")
+    @Test
+    public void testLargeDistance() {
+        Geo point1 = new Geo(0.0, 0.0);
+        Geo point2 = new Geo(0.0, Math.PI / 2); 
+        long actualDistance = vincentyTest.between(point1, point2, radiusTest);
+
+        assertEquals(5777L, actualDistance);
+    }
+    @DisplayName("chrisc23:Test Vincenty calculator for negative degrees")
+    @Test
+    public void testVincentyPartialNegativedegree() {
+
+        Geo geoTest3 = new Geo(Math.toRadians(-30.0), Math.toRadians(-60.0)); 
+        Geo geoTest4 = new Geo(Math.toRadians(30.0), Math.toRadians(60.0)); 
+
+        long actualDistance = vincentyTest.between(geoTest3, geoTest4, radiusTest);
+
+        assertEquals(8261L, actualDistance);
+    }
+    @DisplayName("chrisc23:Test Haversine calculator for all negative degrees")
+    @Test
+    public void testVincentyAllNegativedegree() {
+        Geo geoTest3 = new Geo(Math.toRadians(-30.0), Math.toRadians(-60.0)); 
+        Geo geoTest4 = new Geo(Math.toRadians(-10.0), Math.toRadians(-90.0)); 
+
+        long actualDistance = vincentyTest.between(geoTest3, geoTest4, radiusTest);
+
+        assertEquals(2206L, actualDistance);
+    }
+    @DisplayName("chrisc23: Test Vincenty Calculator: 180 degrees")
+    @Test
+    public void testLarge180Distance() {
+        Geo point1 = new Geo(0.0, 0.0);
+        Geo point2 = new Geo(0.0, Math.PI); 
+        long actualDistance = vincentyTest.between(point1, point2, radiusTest);
+
+        assertEquals(11555L, actualDistance);
+    }
+
 }
