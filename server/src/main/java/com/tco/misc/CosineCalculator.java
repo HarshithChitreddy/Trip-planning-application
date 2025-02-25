4708a210a201f1402f0a91024f48e21afbb7f959
Harshith Reddy Chitreddy <reddy17@colostate.edu>
2025-02-25 15:47:24 -0700

    Created CosinesCalculator class

---
+++ server/src/main/java/com/tco/misc/CosineCalculator.java
@@ -0,0 +1,13 @@
+package com.tco.misc;
+
+import com.tco.misc.DistanceCalculator;
+import com.tco.misc.GeographicCoordinate;
+import com.tco.requests.Places;
+
+public class CosinesCalculator implements DistanceCalculator {
+
+    public Long between(GeographicCoordinate start, GeographicCoordinate end, Double radius) {
+        // Implementation will be done in another PR
+        return null;
+    }
+}
