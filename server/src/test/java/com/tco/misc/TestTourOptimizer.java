6174f8aeb0db8cb6b75816115420ff72afb81df8
Christian Calderon <Chrisc23@colostate.edu>
2025-03-11 20:30:00 -0600

    Added an empty test class

---
+++ server/src/test/java/com/tco/misc/TestTourOptimizer.java
@@ -0,0 +1,12 @@
+package com.tco.misc;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestTourOptimizer {
+    
+}
