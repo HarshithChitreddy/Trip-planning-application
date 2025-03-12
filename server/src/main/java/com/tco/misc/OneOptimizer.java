996c9e47b05c641db29f27511d6886144ca7f81c
Kjell Falk <kjell@berlin.cs.colostate.edu>
2025-03-12 09:15:34 -0600

    Modified OneOptimizer to inherit abstract class TourOptimizer, with overrided method stubs

--- server/src/main/java/com/tco/misc/OneOptimizer.java
+++ server/src/main/java/com/tco/misc/OneOptimizer.java
@@ -1,5 +1,16 @@
 package com.tco.misc;
 
-public class OneOptimizer {
+import com.tco.requests.Places;
 
+public class OneOptimizer extends TourOptimizer {
+
+    @Override
+    public Places construct(Places places, Double radius, String formula, Double response) {
+        return null;
+    }
+
+    @Override
+    public void improve() {
+
+    }
 }
