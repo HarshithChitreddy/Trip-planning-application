505686a49934799e48214b303ef037409f9648d5
Dakota Weathers <dnweath@colostate.edu>
2025-03-11 19:01:18 -0600

    defined CalculatorFactory get method, test case for null input

--- server/src/test/java/com/tco/misc/TestCalculatorFactory.java
+++ server/src/test/java/com/tco/misc/TestCalculatorFactory.java
@@ -4,7 +4,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestCalculatorFactory {
+    private CalculatorFactory testFactory;
     
+    @BeforeEach
+    public void setUp() {
+        testFactory = new CalculatorFactory();
+    }
+
+    @DisplayName("dnweath: test null input returns VincentyCalculator")
+    @Test
+    public void testNullString() throws BadRequestException {
+        DistanceCalculator testCalculator = testFactory.get(null);
+        assertTrue(testCalculator instanceof VincentyCalculator);
+    }
 }
