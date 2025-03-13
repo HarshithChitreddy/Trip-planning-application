f649e4b820baee474fd4320e2988fb51eef3d91f
lennoxxx <you@example.com>
2025-03-13 14:24:24 -0600

    Created getTourOptimizer

--- server/src/main/java/com/tco/misc/OptimizerFactory.java
+++ server/src/main/java/com/tco/misc/OptimizerFactory.java
@@ -1,5 +1,20 @@
 package com.tco.misc;
 
 public class OptimizerFactory {
-    
+
+    public TourOptimizer get(int N, Double response){
+
+        if(response == 0.0){
+            return new NoOptimizer();
+        }
+        else if(N > 250 && N < 600){
+            return new OneOptimizer();
+        }
+        else if(N <= 250){
+            return new TwoOptimizer();
+        }
+        else{ return new NoOptimizer(); }
+    }
+
+
 }
