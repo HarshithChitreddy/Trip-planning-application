875fa6281358e249f2ff06af4d9e2fcd5ae6b962
Christian Calderon <Chrisc23@colostate.edu>
2025-03-13 16:02:16 -0600

    Implemented the construct method for NoOptimization and added some test cases

--- server/src/main/java/com/tco/misc/NoOptimizer.java
+++ server/src/main/java/com/tco/misc/NoOptimizer.java
@@ -6,7 +6,7 @@ public class NoOptimizer extends TourOptimizer {
 
     @Override
     public Places construct(Places places, Double radius, String formula, Double response) {
-        return null;
+        return places;
     }
 
     @Override
--- server/src/test/java/com/tco/misc/TestNoOptimizer.java
+++ server/src/test/java/com/tco/misc/TestNoOptimizer.java
@@ -1,11 +1,59 @@
 package com.tco.misc;
 
 import org.junit.jupiter.api.Test;
+
+import com.tco.requests.Place;
+import com.tco.requests.Places;
+
 import org.junit.jupiter.api.DisplayName;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class TestNoOptimizer {
+import org.junit.jupiter.api.BeforeEach;
 
+public class TestNoOptimizer {
+    private Places places;
+
+    @BeforeEach
+    public void setUp() {
+        places = new Places();
+    }
+    private Place createPlace(String lat, String lon) {
+        Place place = new Place(); 
+        place.put("latitude", lat);
+        place.put("longitude", lon);
+        return place;
+    }
+    @DisplayName("chrisc23: test case for one place")
+    @Test
+    public void testConstructReturnsOriginalPlacesForOnePlace() {
+        
+        places.add(createPlace("100.0", "100.0"));  
+        NoOptimizer noOptimizer = new NoOptimizer();
+        Places result = noOptimizer.construct(places, 10.0, "someFormula", 0.0);
+        assertEquals(places, result);  
+    }
+    @DisplayName("chrisc23: test case for Two place")
+    @Test
+    public void testConstructReturnsOriginalPlacesForTwoPlaces() {
+        
+        places.add(createPlace("100.0", "100.0"));  
+        places.add(createPlace("-100.0", "100.0"));
+        NoOptimizer noOptimizer = new NoOptimizer();
+        Places result = noOptimizer.construct(places, 10.0, "someFormula", 0.0);
+        assertEquals(places, result);  
+    }
+    @DisplayName("chrisc23: test case for Three place")
+    @Test
+    public void testConstructReturnsOriginalPlacesForThreePlaces() {
+        places.add(createPlace("100.0", "-100.0"));  
+        places.add(createPlace("100.0", "100.0"));  
+        places.add(createPlace("-100.0", "100.0"));
+        NoOptimizer noOptimizer = new NoOptimizer();
+        Places result = noOptimizer.construct(places, 10.0, "someFormula", 0.0);
+        assertEquals(places, result);  
+    }
+    
 }
