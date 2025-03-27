d08353286c613158f2580c35e6e4e3112ed836e5
lennoxxx <you@example.com>
2025-03-27 01:07:57 -0600

    converted abstract method to a normal method within tourOptimizer/created test class/case

--- server/src/main/java/com/tco/misc/TourOptimizer.java
+++ server/src/main/java/com/tco/misc/TourOptimizer.java
@@ -1,9 +1,25 @@
 package com.tco.misc;
 import com.tco.requests.Places;
 public abstract class TourOptimizer {
+    private Places currentTour;
+    private double earthRadius;
+    private long startTime;
+    private double responseTime;
+    private DistanceCalculator calculator;
 
-    public abstract Places construct(Places places, Double radius, String formula, Double response);
+    public Places construct(Places places, Double radius, String formula, Double response){
+        if (places == null || places.size() < 3) {
+            return places;
+        }
+        
+        this.currentTour = new Places();
+        this.currentTour.addAll(places);
+        this.earthRadius = radius;
+        this.responseTime = response * 1000; 
+        this.startTime = System.currentTimeMillis();
+        
+        return currentTour;
+    }
 
     public abstract void improve();
-    
 }
--- server/src/test/java/com/tco/misc/TestTourOptimizer.java
+++ server/src/test/java/com/tco/misc/TestTourOptimizer.java
@@ -2,11 +2,55 @@ package com.tco.misc;
 
 
 import org.junit.jupiter.api.Test;
+
+import com.tco.requests.Place;
+import com.tco.requests.Places;
+
 import org.junit.jupiter.api.DisplayName;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class TestTourOptimizer {
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestTourOptimizer extends TourOptimizer{
+    @Override
+    public Places construct(Places places, Double radius, String formula, Double response){
+        return null;
+    }
+    @Override
+    public void improve() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'improve'");
+    }
+
+    private TourOptimizer tourOpt;
+    private Places testPlaces;
+    private Place place1 = new Place();
+    private Place place2 = new Place();
+    private Place place3 = new Place();
+    
+
+
+    @BeforeEach
+    public void setUp(){
+        tourOpt = new TestTourOptimizer();
+        testPlaces = new Places();
+
+        place1.put("latitude", "0.0");
+        place1.put("longitude", "120.0");
+        place2.put("latitude", "0.0");
+        place2.put("longitude", "0.0");
+        place3.put("latitude", "-90.0");
+        place3.put("longitude", "0.0");
+    }
+
+    @Test
+    @DisplayName("lennoxxx: Test when constructing null Places list.")
+    public void testConstructNullPlaces(){
+        Places result = tourOpt.construct(null, 4300.0, "haversine", 1.0);
+        assertNull(result);
+    }
     
 }
