84179b0cacc5f324664792aaf8d9b5535100b928
Dakota Weathers <dnweath@colostate.edu>
2025-04-07 17:32:42 -0600

    created GeographicLocations.java shell

---
+++ server/src/test/java/com/tco/misc/TestGeographicLocations.java
@@ -0,0 +1,13 @@
+package com.tco.misc;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestGeographicLocations {
+    
+}
