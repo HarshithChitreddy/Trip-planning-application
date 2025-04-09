896db568b686251a11c186b24e9954a3e282992b
lennoxxx <you@example.com>
2025-04-08 23:29:09 -0600

    Added necesarry class variables

---
+++ server/src/main/java/com/tco/requests/NearRequest.java
@@ -0,0 +1,12 @@
+package com.tco.requests;
+
+public class NearRequest {
+    private Places places;
+    private Place place;
+    private Distances distances;
+    private int distance;
+    protected double earthRadius;
+    protected String formula;
+    protected int limit;
+    
+}
