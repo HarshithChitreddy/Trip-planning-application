8d330c532be13f271fe7162faa7429f9d64c9b0e
lennoxxx <you@example.com>
2025-04-09 22:17:12 -0600

    created shell for threeOpt, extended, methods, class vars

--- server/src/main/java/com/tco/misc/ThreeOptimizer.java
+++ server/src/main/java/com/tco/misc/ThreeOptimizer.java
@@ -1,3 +1,23 @@
-public class ThreeOptimizer {
+package com.tco.misc;
+
+import com.tco.requests.Place;
+import com.tco.requests.Places;
+
+public class ThreeOptimizer extends TourOptimizer{
+    private Places currentTour;
+    private double earthRadius;
+    private long startTime;
+    private double responseTime;
+    private DistanceCalculator calculator;
     
+    public ThreeOptimizer(Places places, double radius, double responseSeconds, DistanceCalculator calculator){
+        if (currentTour == null || currentTour.size() < 3 || calculator == null) {
+            return;
+        }
+    }
+
+    @Override
+    public void improve(){
+
+    }
 }
