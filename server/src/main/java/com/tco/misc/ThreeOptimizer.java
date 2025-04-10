d8c8a8fcb55235fc562e5d47e213d73197e155dc
Harshith Reddy Chitreddy <reddy17@colostate.edu>
2025-04-09 22:41:25 -0600

    Define ThreeOptimizer

--- server/src/main/java/com/tco/misc/ThreeOptimizer.java
+++ server/src/main/java/com/tco/misc/ThreeOptimizer.java
@@ -3,25 +3,33 @@ package com.tco.misc;
 import com.tco.requests.Place;
 import com.tco.requests.Places;
 
-public class ThreeOptimizer extends TourOptimizer{
+public class ThreeOptimizer extends TourOptimizer {
+
     private Places currentTour;
     private double earthRadius;
     private long startTime;
     private double responseTime;
     private DistanceCalculator calculator;
-    
-    public ThreeOptimizer(Places places, double radius, double responseSeconds, DistanceCalculator calculator){
-        if (currentTour == null || currentTour.size() < 3 || calculator == null) {
-            return;
+
+    public ThreeOptimizer(Places places, double radius, String formula, double response) throws BadRequestException {
+        this.currentTour = (places == null || places.size() < 3) ? places : new Places();
+        if (places != null && places.size() >= 3) {
+            this.currentTour.addAll(places);
         }
+
+        this.earthRadius = radius;
+        this.responseTime = response * 1000;
+        this.calculator = new CalculatorFactory().get(formula); 
+        this.startTime = System.currentTimeMillis();
     }
 
     @Override
-    public void improve(){
-
+    public void improve() {
+        // To be implemented in next pr
+        System.out.println("ThreeOptimizer improve() method defined.");
     }
 
-    public Places getOptimizedTour(){
+    public Places getOptimizedTour() {
         return currentTour;
     }
 }
