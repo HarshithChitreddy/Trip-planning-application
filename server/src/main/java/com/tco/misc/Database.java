5bd92b88c7aed2b780708d756a3eea4b22e3a063
Dakota Weathers <dnweath@colostate.edu>
2025-04-17 13:17:08 -0600

    Add Database places shell

--- server/src/main/java/com/tco/misc/Database.java
+++ server/src/main/java/com/tco/misc/Database.java
@@ -1,3 +1,17 @@
+package com.tco.misc;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.Statement;
+
+import com.tco.requests.Place;
+import com.tco.requests.Places;
+
 public class Database {
-    //shell class
+    private final static String COLUMNS = "city,city_ascii,city_alt,lat,lng,country,admin_name,admin_name_ascii,admin_type";
+
+    static Places places(String sql, Integer limit) throws Exception {
+        return new Places();
+    }
 }
