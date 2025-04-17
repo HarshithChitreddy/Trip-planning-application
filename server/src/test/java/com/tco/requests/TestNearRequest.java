3857fbb39eca7ebde259406eed7db5ae8b0ebd9e
lennoxxx <you@example.com>
2025-04-17 15:27:16 -0600

    Declared test class for near req

---
+++ server/src/test/java/com/tco/requests/TestNearRequest.java
@@ -0,0 +1,36 @@
+package com.tco.requests;
+
+import org.junit.jupiter.api.Test;
+
+import com.tco.misc.BadRequestException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class TestNearRequest {
+    
+    @BeforeEach
+    public void setup(){
+
+    }
+
+    // @Test
+    // @DisplayName("netID: Test for...")
+    // public void test() throws BadRequestException{
+    // } 
+
+    // @Test
+    // @DisplayName("netID: Test for...")
+    // public void test() throws BadRequestException{
+    // } 
+
+    // @Test
+    // @DisplayName("netID: Test for...")
+    // public void test() throws BadRequestException{
+    // } 
+    
+}
