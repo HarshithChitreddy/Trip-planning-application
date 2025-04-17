356f3d8a97574dfc39736f077f431c92e27fad0c
Dakota Weathers <dnweath@colostate.edu>
2025-04-17 14:32:33 -0600

    Add distances method to GeoLoc with tests

--- server/src/main/java/com/tco/misc/GeographicLocations.java
+++ server/src/main/java/com/tco/misc/GeographicLocations.java
@@ -2,15 +2,34 @@ package com.tco.misc;
 
 import com.tco.requests.Place;
 import com.tco.requests.Places;
+import com.tco.requests.Distances;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GeographicLocations {
-    public Places near(Place place, long distance, double earthRadius, String formula, int limit) {
+    CalculatorFactory calculatorFactory;
+    DistanceCalculator calculator;
+    double earthRadius;
+    
+    public Places near(Place place, long distance, double earthRadius, String formula, int limit) throws BadRequestException {
         Places nearPlaces = new Places();
 
+        this.calculatorFactory = new CalculatorFactory();
+        this.calculator = calculatorFactory.get(formula);
+        this.earthRadius = earthRadius;
+
         return nearPlaces;
+    }
+
+    public Distances distances(Place place, Places places) {
+        Distances distances = new Distances();
+        
+        for (Place eachPlace : places) {
+            distances.add(calculator.between(place, eachPlace, earthRadius));
+        }
+
+        return distances;
     }
 
     public List<String> getTypes() {
