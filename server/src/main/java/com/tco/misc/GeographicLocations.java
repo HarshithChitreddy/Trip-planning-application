b7af2b63096cffbf64b6c5da007e7bed3241c248
Harshith Reddy Chitreddy <reddy17@colostate.edu>
2025-04-17 13:16:56 -0600

    Implement getTypes method

--- server/src/main/java/com/tco/misc/GeographicLocations.java
+++ server/src/main/java/com/tco/misc/GeographicLocations.java
@@ -3,10 +3,19 @@ package com.tco.misc;
 import com.tco.requests.Place;
 import com.tco.requests.Places;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GeographicLocations {
     public Places near(Place place, long distance, double earthRadius, String formula, int limit) {
         Places nearPlaces = new Places();
 
         return nearPlaces;
+    }
+
+    public List<String> getTypes() {
+        List<String> types = new ArrayList<>();
+        types.add("city");
+        return types;
     }
 }
