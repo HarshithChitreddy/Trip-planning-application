065610a2a6931928ac0e4efbeb1819c1223e6537
lennoxxx <you@example.com>
2025-05-01 17:17:37 -0600

    removed setter for limit

--- server/src/main/java/com/tco/requests/FindRequest.java
+++ server/src/main/java/com/tco/requests/FindRequest.java
@@ -18,9 +18,7 @@ public class FindRequest extends Request{
 public void buildResponse() throws BadRequestException{
     
 }
-public void setLimit(Integer limit){
-    this.limit = limit;
-}
+
 public Integer getLimit(){
     return limit;
 }
