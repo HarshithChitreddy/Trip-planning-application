f0be3659765d51b249478ef36fac180549fdb68d
Harshith Reddy Chitreddy <reddy17@colostate.edu>
2025-05-01 15:59:55 -0600

    adding-new-testcases-to-nearrequest

--- server/src/test/java/com/tco/requests/TestNearRequest.java
+++ server/src/test/java/com/tco/requests/TestNearRequest.java
@@ -1,36 +1,75 @@
 package com.tco.requests;
 
 import org.junit.jupiter.api.Test;
-
-import com.tco.misc.BadRequestException;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.tco.misc.BadRequestException;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestNearRequest {
-    
-    @BeforeEach
-    public void setup(){
 
+    private NearRequest request;
+
+    @BeforeEach
+    public void setup() {
+        request = new NearRequest();
+        request.setDistance(300);
+        request.setEarthRadius(3959.0);
+        request.setFormula("great-circle");
+        request.setLimit(5);
     }
 
-    // @Test
-    // @DisplayName("netID: Test for...")
-    // public void test() throws BadRequestException{
-    // } 
+    @Test
+    @DisplayName("reddy17: Longitude below -180 should throw BadRequestException")
+    public void testLongitudeTooLow() {
+        Place invalidPlace = new Place();
+        invalidPlace.put("latitude", "40.0");
+        invalidPlace.put("longitude", "-181.0");
 
-    // @Test
-    // @DisplayName("netID: Test for...")
-    // public void test() throws BadRequestException{
-    // } 
+        setPrivateField(request, "place", invalidPlace);
 
-    // @Test
-    // @DisplayName("netID: Test for...")
-    // public void test() throws BadRequestException{
-    // } 
-    
+        assertThrows(BadRequestException.class, () -> {
+            request.buildResponse();
+        });
+    }
+
+    @Test
+    @DisplayName("reddy17: Longitude above 180 should throw BadRequestException")
+    public void testLongitudeTooHigh() {
+        Place invalidPlace = new Place();
+        invalidPlace.put("latitude", "40.0");
+        invalidPlace.put("longitude", "181.0");
+
+        setPrivateField(request, "place", invalidPlace);
+
+        assertThrows(BadRequestException.class, () -> {
+            request.buildResponse();
+        });
+    }
+
+    @Test
+    @DisplayName("reddy17: Valid longitude should not throw error")
+    public void testValidLongitude() {
+        Place validPlace = new Place();
+        validPlace.put("latitude", "40.0");
+        validPlace.put("longitude", "100.0");
+
+        setPrivateField(request, "place", validPlace);
+
+        assertDoesNotThrow(() -> {
+            request.buildResponse();
+        });
+    }
+
+    private void setPrivateField(NearRequest request, Place place) {
+        try {
+            java.lang.reflect.Field field = NearRequest.class.getDeclaredField("place");
+            field.setAccessible(true);
+            field.set(request, place);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
