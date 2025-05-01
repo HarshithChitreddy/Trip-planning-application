6211819d5d98505c30ab8a2e9349efe86a7c7fc0
lennoxxx <you@example.com>
2025-05-01 17:44:57 -0600

    created outline for FindReq Test class

---
+++ server/src/test/java/com/tco/requests/TestFindRequest.java
@@ -0,0 +1,17 @@
+package com.tco.requests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindRequestTest {
+
+    private FindRequest request;
+
+    @BeforeEach
+    void setUp() {
+        request = new FindRequest();
+    }
+
+}
